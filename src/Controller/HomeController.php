c371322df1762025b074f7866d40b1e299388d68
Hugo Melin <hugo.melin01@gmail.com>
2024-08-30 08:22:56 +0200

    Add top section for sweatshirts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SweatShirtRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home.index')]
-    public function index(): Response
+    public function index(SweatShirtRepository $repository): Response
     {
+        $sweats = $repository->showTop();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'Accueil',
+            'sweats'=> $sweats,
         ]);
     }
 }
